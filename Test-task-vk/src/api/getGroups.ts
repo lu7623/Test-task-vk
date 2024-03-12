a6bc76633c671c57012c7ace8330969c426f46a5
Lyubov Agulova <lu7623@gmail.com>
2024-03-12 16:13:20 +0300

    feat: add getGroups function

---
+++ Test-task-vk/src/api/getGroups.ts
@@ -0,0 +1,18 @@
+import groups from "./groups.json" assert { type: "json" };
+
+function timeout(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+function getGroups() {
+  const res: GetGroupsResponse = {
+    result: 1,
+    data: groups,
+  };
+
+  return res;
+}
+
+export async function getGroupsWithDelay(): Promise<GetGroupsResponse> {
+  await timeout(1000);
+  return getGroups();
+}
